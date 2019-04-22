83966fd850d55e85ff9fa056a936a4ee8f587326
wangbaoshuai <wangbaoshuai>
2019-04-22 16:41:04 +0800

    add a central cleanup timer

--- cache.go
+++ cache.go
@@ -7,30 +7,15 @@ import (
 )
 
 /*
-Rating system designed to be used in VoIP Carriers World
-Copyright (C) 2012  Radu Ioan Fericean
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
-
-/*
- Structure that must be embeded in the objectst that must be cached with expiration.
- If the expiration is not needed this can be ignored
+ Structure that must be embedded in the object that should be cached with expiration
+ If no expiration is desired this can be ignored
  */
 type XEntry struct {
 	sync.Mutex                   //互斥锁
 	key            string        //key
 	keepAlive      bool          //是否保持连接
 	expireDuration time.Duration //到期时间
-	t              *time.Timer   //定时器
+	expiringSince  time.Time     //时间
 }
 
 /**
@@ -38,16 +23,53 @@ type XEntry struct {
  */
 type expiringCacheEntry interface {
 	XCache(key string, expire time.Duration, value expiringCacheEntry)
-	timer() *time.Timer
 	KeepAlive()
+	ExpiringSince() time.Time
+	ExpireDuration() time.Duration
 }
 
 var (
-	xcache = make(map[string]expiringCacheEntry)
-	xMux   sync.RWMutex
-	cache  = make(map[string]interface{})
-	mux    sync.RWMutex
+	xcache              = make(map[string]expiringCacheEntry)
+	cache               = make(map[string]interface{})
+	xMux                sync.RWMutex
+	mux                 sync.RWMutex
+	expTimer            *time.Timer
+	ExpireCheckInterval = 30 * time.Second
 )
+
+/**
+  init
+ */
+func Init() {
+	expirationCheck()
+}
+
+/**
+   check
+ */
+func expirationCheck() {
+	go removeExpiredEntries()
+}
+
+/**
+  remove
+ */
+func removeExpiredEntries() {
+	//Take a copy of xcache so we can iterate over it without blocking the mutex
+	xMux.Lock()
+	cache := xcache
+	xMux.Unlock()
+
+	now := time.Now()
+	for key, c := range cache {
+		if now.Sub(c.ExpiringSince()) >= c.ExpireDuration() {
+			xMux.Lock()
+			delete(xcache, key)
+			xMux.Unlock()
+		}
+	}
+	expTimer = time.AfterFunc(ExpireCheckInterval, expirationCheck)
+}
 
 /**
   The main function to cache with expiration
@@ -56,37 +78,38 @@ func (xe *XEntry) XCache(key string, expire time.Duration, value expiringCacheEn
 	xe.keepAlive = true
 	xe.key = key
 	xe.expireDuration = expire
+	xe.expiringSince = time.Now()
+
 	xMux.Lock()
+	defer xMux.Unlock()
 	xcache[key] = value
-	xMux.Unlock()
-	go xe.expire()
 }
 
 /**
-  The internal mechanism for expiartion
- */
-func (xe *XEntry) expire() {
-	for xe.keepAlive {
-		xe.Lock()
-		xe.keepAlive = false
-		xe.Unlock()
-		xe.t = time.NewTimer(xe.expireDuration) //设置到期时间
-		<-xe.t.C
-		if !xe.keepAlive {
-			xMux.Lock()
-			delete(xcache, xe.key)
-			xMux.Unlock()
-		}
-	}
-}
-
-/**
-   Mark entry to be kept another expirationDuration period
+   Mark entry to be kept for another expirationDuration period
  */
 func (xe *XEntry) KeepAlive() {
 	xe.Lock()
 	defer xe.Unlock()
-	xe.keepAlive = true
+	xe.expiringSince = time.Now()
+}
+
+/**
+  Returns this entry's expiration duration
+ */
+func (xe *XEntry) ExpireDuration() time.Duration {
+	xe.Lock()
+	defer xe.Unlock()
+	return xe.expireDuration
+}
+
+/**
+  Returns since when this entry is expiring
+ */
+func (xe *XEntry) ExpiringSince() time.Time {
+	xe.Lock()
+	defer xe.Unlock()
+	return xe.expiringSince
 }
 
 /**
@@ -99,11 +122,11 @@ func Cache(key string, value interface{}) {
 }
 
 /**
-  Get an entry from the expiration cache and mark it for keeping alive
+  Get an entry from the expiration cache and mark it to be kept alive
  */
 func GetXCached(key string) (ece expiringCacheEntry, err error) {
 	xMux.RLock()
-	defer xMux.Unlock()
+	defer xMux.RUnlock()
 	if r, ok := xcache[key]; ok {
 		r.KeepAlive() //Start calling connection
 		return r, nil
@@ -112,7 +135,7 @@ func GetXCached(key string) (ece expiringCacheEntry, err error) {
 }
 
 /**
-   The function to extract a value for a key that never expire
+   The function to extract a value for a key that never expires
  */
 func GetCached(key string) (v interface{}, err error) {
 	mux.RLock()
@@ -124,14 +147,7 @@ func GetCached(key string) (v interface{}, err error) {
 }
 
 /**
-  Getter for the timer
- */
-func (xe *XEntry) timer() *time.Timer {
-	return xe.t
-}
-
-/**
-  Delete all keys from cache
+  Delete all keys from expiration cache
  */
 func Flush() {
 	mux.Lock()
@@ -140,15 +156,10 @@ func Flush() {
 }
 
 /**
-
+  Delete all keys from cache
  */
 func XFulsh() {
 	xMux.Lock()
 	defer xMux.Unlock()
-	for _, v := range xcache {
-		if v.timer() != nil {
-			v.timer().Stop()
-		}
-	}
 	xcache = make(map[string]expiringCacheEntry)
 }
